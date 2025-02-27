d1dabef37ce9d0de477ca269dad52b4f702d7ef0
joonwoo595 <jonbeo312@gmail.com>
2025-02-27 16:59:41 +0900

    니나노닷컴

--- src/main/java/com/wecompany/choongangihwa/controller/FirstController.java
+++ src/main/java/com/wecompany/choongangihwa/controller/FirstController.java
@@ -1,9 +1,14 @@
 package com.wecompany.choongangihwa.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
 
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
 
 }
